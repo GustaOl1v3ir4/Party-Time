43eaa9d0b994df6c63efa13fe810ee28b6b212aa
GustaOl1v3ir4 <96428462+GustaOl1v3ir4@users.noreply.github.com>
2025-12-10 22:21:23 -0300

    Ajustando a porta para subir em produção

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,22 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
-
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // DB Connection
 const conn = require("./db/conn");
-
 conn();
 
-//Routes
+// Routes
 const routes = require("./routes/router");
-
 app.use("/api", routes);
 
-app.listen(3000, function() {
-    console.log("Servidor Rodando")
-})
+// Porta correta para produção
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log("Servidor rodando na porta " + PORT);
+});
+
